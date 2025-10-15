22dec762270358a0efd75f060979f386bed07967
zeeumsiraji <zeeumsiraji@gmail.com>
2025-10-15 09:40:07 +0600

    Create SignUp App

---
+++ app/src/main/java/com/example/fristsignup/LoginPage.kt
@@ -0,0 +1,32 @@
+package com.example.fristsignup
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginPage : AppCompatActivity() {
+    private lateinit var RegesterBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        RegesterBtn = findViewById(R.id.btnRegester)
+        RegesterBtn.setOnClickListener {
+            val intent = Intent(this, HomePage::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/fristsignup/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.fristsignup
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var reqBtn: Button
+    private lateinit var loginBtn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        reqBtn = findViewById(R.id.btnSignUp)
+        reqBtn.setOnClickListener {
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+        }
+        loginBtn = findViewById(R.id.btnLogin)
+        // ✅ Correct Intent usage
+        loginBtn.setOnClickListener {
+            val intent = Intent(this,LoginPage::class.java)
+            startActivity(intent)
+        }
+    }
+}
